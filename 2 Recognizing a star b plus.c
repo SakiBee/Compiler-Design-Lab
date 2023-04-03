77f0ec0b1c16bdf6895ca8bba3b87b7e06c282a8
Md. Sakib Uddin <63546044+SakiBee@users.noreply.github.com>
2023-04-04 00:35:12 +0600

    Update 2 Recognizing a star b plus.c

--- 2 Recognizing a star b plus.c
+++ 2 Recognizing a star b plus.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 int main() {
-	char str[1000];
-	gets(str);
-	if(strcmp(str, "a") == 0) {
-		printf("This string is recognized under 'a' and 'a*b+'\n");
-	}
-	else if(strcmp(str, "abb") == 0) {
-		printf("This string is recognized under 'abb' and 'a*b+'\n");
-	}
-	else {
-		int len = strlen(str);
-		int i = 0; 
-		while(i < len) {
-			if(str[i] == 'a') i++;
-			else break;
-		}
-		while(i < len) {
-			if(str[i] == 'b') i++;
-			else break;
-		}
-		if(i != len) {
-			printf("Invalid String");
-		}
-		else{
-			printf("This string is recognized under 'a*b+'\n");
-		}
-		
-	}
+  char str[1000];
+  gets(str);
+  int len = strlen(str), i;
+  bool invalid = 0;
+  if(str[0] == 'b') {
+    int i = 0;
+    while(i < len and str[i] == 'b') i++;
+    if(i != len) invalid = 1;
+  }
+  else {
+    i = 0; 
+    while(i < len and str[i] == 'a') i++;
+    while(i < len and str[i] == 'b') i++;
+    if(i != len) invalid = 1;    
+  }
+
+  if(invalid) printf("Invalid String");
+  else printf("This string is recognized under 'a*b+'\n");
 }
